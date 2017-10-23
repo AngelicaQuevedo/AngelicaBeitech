0563c0ed5c228859799d866d8a6b947813623e83
PSLCOL\jpachonv <jpachonv@psl.com.co>
2017-10-23 10:47:53 -0500

    Get allowed products service modification

--- AngelicaRestService/src/main/java/com/angelica/model/Customer.java
+++ AngelicaRestService/src/main/java/com/angelica/model/Customer.java
@@ -4,73 +4,61 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
 @Entity
-@Table(name="CUSTOMER")
-public class Customer{
+@Table(name = "CUSTOMER")
+public class Customer {
 
     @Id
     @Column(name = "CUSTOMER_ID")
-	private BigInteger customerId;
+    private BigInteger customerId;
 
     @Column(name = "NAME")
     private String name;
 
     @Column(name = "EMAIL")
     private String email;
-    
-//    @ManyToMany(cascade = CascadeType.ALL)
-//    @LazyCollection(LazyCollectionOption.FALSE)
-//    @JoinTable(name = "ALLOWED_PRODUCT_CUSTOMER", 
-//    		joinColumns = { @JoinColumn(name = "customer_id") }, 
-//    		inverseJoinColumns = { @JoinColumn(name = "product_id") })
-//    private List<Product> products = new ArrayList<>();
+
     @ManyToMany
     private List<Product> allowedProducts = new ArrayList<>();
 
-	public BigInteger getCustomerId() {
-		return customerId;
-	}
+    public BigInteger getCustomerId() {
+        return customerId;
+    }
 
-	public void setCustomerId(BigInteger customerId) {
-		this.customerId = customerId;
-	}
+    public void setCustomerId(BigInteger customerId) {
+        this.customerId = customerId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public List<Product> getProducts() {
-		return allowedProducts;
-	}
+    public List<Product> getProducts() {
+        return allowedProducts;
+    }
 
-	public void setProducts(List<Product> products) {
-		this.allowedProducts = products;
-	}
+    public void setProducts(List<Product> products) {
+        this.allowedProducts = products;
+    }
 
-	
+
 
 }
--- AngelicaRestService/src/main/java/com/angelica/model/Product.java
+++ AngelicaRestService/src/main/java/com/angelica/model/Product.java
@@ -1,5 +1,7 @@
 package com.angelica.model;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,86 +14,78 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="PRODUCT")
+@Table(name = "PRODUCT")
 public class Product {
-	
-	@Id
-	@GeneratedValue
-	@Column(name = "product_id")
-	private Long productId;
-	
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "description")
-	private String description;
-	
-	@Column(name = "creation_date")
-	private Date creationDate;
-	
-	@Column(name = "price")
-	private Integer price;
-	
-	@Column(name = "category_id")
-	private Integer categoryId;
-	
-	@ManyToMany(mappedBy="allowedProducts")
-	private List<Customer> customers = new ArrayList<>();
 
-	public Long getProductId() {
-		return productId;
-	}
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private BigInteger productId;
 
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
+    @Column(name = "name")
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    @Column(name = "description")
+    private String description;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Column(name = "creation_date")
+    private Date creationDate;
 
-	public String getDescription() {
-		return description;
-	}
+    @Column(name = "price")
+    private BigDecimal price;
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    @Column(name = "category_id")
+    private BigInteger categoryId;
 
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    @ManyToMany(mappedBy = "allowedProducts")
+    private List<Customer> customers = new ArrayList<>();
 
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
+    public BigInteger getProductId() {
+        return productId;
+    }
 
-	public Integer getPrice() {
-		return price;
-	}
+    public void setProductId(BigInteger productId) {
+        this.productId = productId;
+    }
 
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Integer getCategoryId() {
-		return categoryId;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setCategoryId(Integer categoryId) {
-		this.categoryId = categoryId;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-//	public List<Customer> getCustomers() {
-//		return customers;
-//	}
-//
-//	public void setCustomers(List<Customer> customers) {
-//		this.customers = customers;
-//	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigInteger getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(BigInteger categoryId) {
+        this.categoryId = categoryId;
+    }
 
 }
