42ac0d584b34354a71d682a281416ccf8fba0ecb
Angelica Q <Angelica Q@Angelica>
2017-10-23 00:51:48 -0500

    Rest services for orders

--- AngelicaRestService/src/main/java/com/angelica/webservice/restapi/OrderRestController.java
+++ AngelicaRestService/src/main/java/com/angelica/webservice/restapi/OrderRestController.java
@@ -3,6 +3,7 @@ package com.angelica.webservice.restapi;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +18,18 @@ public class OrderRestController {
     @RequestMapping(value = "/AddOrder", method = RequestMethod.POST)
     public OrderResponse addOrder(@RequestBody AddOrderRequest orderReq) {
     	OrderResponse response = new OrderResponse();
-        response.returnCode = 0;
-        response.returnMessage = "exito";
+    	try{
+            response.returnCode = 0;
+            response.returnMessage = "success";
+    	}catch(Exception e){
+    		response.returnCode = 1;
+    		response.returnMessage = "Exception during adding the order";
+    	}
         return response;
     }
     
-    @RequestMapping(value = "/GetOrdersByCustomer", method = RequestMethod.POST)
-    public OrderResponse getOrdersByCustomer(@RequestBody OrdersByCustomerRequest orderReq){
+    @RequestMapping(value = "/GetOrdersByCustomer/{id}", method = RequestMethod.GET)
+    public OrderResponse getOrdersByCustomer(@RequestBody OrdersByCustomerRequest orderReq, @PathVariable("id") Long customerId){
     	OrderResponse response = new OrderResponse();
     	response.returnCode = 0;
         response.returnMessage = "exito";
@@ -40,7 +46,6 @@ public class OrderRestController {
     }
     
     class OrdersByCustomerRequest{
-    	public Integer customerId;
     	public LocalDate initDate;
     	public LocalDate endDate;
     }
